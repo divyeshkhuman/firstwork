4d15bc3c342b4603253c1e602e212be23e4743ea
khuman <khumandivyesh22@gmail.com>
2023-08-26 22:34:05 +0530

    finished project

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:youtech/loginscreen.dart';
+
+
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(
+    MaterialApp(
+      home: loginscreen(),
+    )
+  );
+}
+
+
+
